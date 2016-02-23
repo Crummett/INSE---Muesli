ad6ae6d4d8c3de56b84b9e8dbc7d729694a61bdd
jahidulpabelislam <jahidul.pabel.islam@hotmail.com>
2016-02-23 11:40:24 +0000

    Created PHP
    Created PHP

---
+++ api/get.php
@@ -0,0 +1,12 @@
+<?php
+	include '../inc/connection.php';
+
+	$query = "SELECT * from task;";
+    $rows = $db->query($query);
+    $rows = $rows->fetchAll(PDO::FETCH_ASSOC);
+    
+    $results = [];
+	$results["rows"] = $rows;
+	header("Content-Type: application/json");
+    echo json_encode($results);
+?>
