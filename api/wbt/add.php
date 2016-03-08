56500b430ed9e1c42464ba6727392f20f5044ec7
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2016-03-08 11:50:05 +0000

    Update to API
    Update to API due to changing database

--- api/wbt/add.php
+++ api/wbt/add.php
@@ -2,17 +2,13 @@
 	include '../../inc/all.php';
 	
 	$Text = $_POST['Text'];
-	$WBTX = $_POST['WBTX'];
-	$WBTY = $_POST['WBTY'];
-	$WBTWidth = $_POST['WBTWidth'];
-	$WBTHeight = $_POST['WBTHeight'];
 	
 	if (isset($_POST['PreseedingID'])) {
 		$PreseedingID = $_POST['PreseedingID'];
-		$query = "INSERT INTO Task (Text, PreseedingID, WBTX, WBTY, WBTWidth, WBTHeight) VALUES ('${Text}', ${PresendingID}, ${WBTX}, ${WBTY}, ${WBTWidth}, ${WBTHeight});";
+		$query = "INSERT INTO Task (Text, PreseedingID) VALUES ('${Text}', ${PresendingID});";
 	}
 	else {
-		$query = "INSERT INTO Task (Text, WBTX, WBTY, WBTWidth, WBTHeight) VALUES ('${Text}', ${WBTX}, ${WBTY}, ${WBTWidth}, ${WBTHeight});";
+		$query = "INSERT INTO Task (Text) VALUES ('${Text}');";
 	}
 	$db->query($query);
 ?>
--- api/wbt/update.php
+++ api/wbt/update.php
@@ -3,17 +3,13 @@
 	
 	$ID = $_POST['ID'];
 	$Text = $_POST['Text'];
-	$WBTX = $_POST['WBTX'];
-	$WBTY = $_POST['WBTY'];
-	$WBTWidth = $_POST['WBTWidth'];
-	$WBTHeight = $_POST['WBTHeight'];
 
 	if (isset($_POST['PreseedingID'])) {
 		$PreseedingID = $_POST['PreseedingID'];
-		$query = "UPDATE Task SET Text = '${Text}', PreseedingID = ${PreseedingID}, WBTX = ${WBTX}, WBTY = ${WBTY}, WBTWidth = ${Width}, WBTHeight = ${Height} WHERE ID = ${ID};";
+		$query = "UPDATE Task SET Text = '${Text}', PreseedingID = ${PreseedingID} WHERE ID = ${ID};";
 	}
 	else {
-		$query = "UPDATE Task SET Text = '${Text}', WBTX = ${WBTX}, WBTY = ${WBTY}, WBTWidth = ${Width}, WBTHeight = ${Height} WHERE ID = ${ID};";
+		$query = "UPDATE Task SET Text = '${Text}' WHERE ID = ${ID};";
 	}
 	
     $db->query($query);
